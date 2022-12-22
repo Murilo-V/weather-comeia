66b4bb6ed30ccce9f9be41dc6a4bacc402ab7f1a
Murilo-V <alveiramurilo@hotmail.com>
2022-12-22 08:59:34 -0300

    feat: dotted grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,28 @@ import { Component } from '@angular/core';
 export class AppComponent {
   gridColsBreakpoint: number = 2;
   gridRowsBreakpoint: string = '4:1';
-  data = [];
+  data = [
+    {
+      city: 'Londres',
+      temp: '4ºC',
+    },
+    {
+      city: 'São Paulo',
+      temp: '21ºC',
+    },
+    {
+      city: 'Buenos Aires',
+      temp: '16ºC',
+    },
+    {
+      city: 'Tokyo',
+      temp: '19ºC',
+    },
+    {
+      city: 'Salvador',
+      temp: '31ºC',
+    },
+  ];
 
   ngOnInit() {
     this.gridColsBreakpoint = window.innerWidth <= 600 ? 1 : 2;
