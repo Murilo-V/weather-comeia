7bbb0e1a34271044a0735c69c6af81ba55de6280
Murilo-V <alveiramurilo@hotmail.com>
2022-12-22 13:42:29 -0300

    feat: json values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface Citie {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,7 +13,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   gridColsBreakpoint: number = 2;
   gridRowsBreakpoint: string = '4:1';
-  data = [
+  selectedCity = 'salvador';
+  cities: Citie[] = [
+    { value: 'rio', viewValue: 'Rio de Janeiro' },
+    { value: 'bh', viewValue: 'Belo Horizonte' },
+    { value: 'paris', viewValue: 'Paris' },
+    { value: 'ny', viewValue: 'Nova York' },
+  ];
+  weathers = [
     {
       city: 'Londres',
       temp: '4ºC',
@@ -34,5 +46,10 @@ export class AppComponent {
   ngOnInit() {
     this.gridColsBreakpoint = window.innerWidth <= 600 ? 1 : 2;
     this.gridRowsBreakpoint = window.innerWidth <= 600 ? '2:1' : '4:1';
+  }
+
+  removeCity(city: string) {
+    this.weathers = this.weathers.filter((w) => w.city !== city);
+    // this.cities.push({ value: '' });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { AppComponent } from './app.component';
 import { MatButtonModule } from '@angular/material/button';
 import { HeaderComponent } from './header/header.component';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
-  imports: [BrowserModule, MatCardModule, MatButtonModule, MatGridListModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatSelectModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
